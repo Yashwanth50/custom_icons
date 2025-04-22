fda96423af415b17eda854a2f4ac79e37e7fcb2d
Yashwanth50 <Yashwanth@uxuistudio.in>
2025-04-22 19:54:15 +0530

    Add all icon sets and fonts

---
+++ project-section-icons-v1.0/liga.js
@@ -0,0 +1,81 @@
+/* A polyfill for browsers that don't support ligatures. */
+/* The script tag referring to this file must be placed before the ending body tag. */
+
+/* To provide support for elements dynamically added, this script adds
+   method 'icomoonLiga' to the window object. You can pass element references to this method.
+*/
+(function () {
+    'use strict';
+    function supportsProperty(p) {
+        var prefixes = ['Webkit', 'Moz', 'O', 'ms'],
+            i,
+            div = document.createElement('div'),
+            ret = p in div.style;
+        if (!ret) {
+            p = p.charAt(0).toUpperCase() + p.substr(1);
+            for (i = 0; i < prefixes.length; i += 1) {
+                ret = prefixes[i] + p in div.style;
+                if (ret) {
+                    break;
+                }
+            }
+        }
+        return ret;
+    }
+    var icons;
+    if (!supportsProperty('fontFeatureSettings')) {
+        icons = {
+            'edit-prompt': '&#xe900;',
+            'redo': '&#xe901;',
+            'undo': '&#xe902;',
+            'expand': '&#xe903;',
+            'link': '&#xe904;',
+            'alignment-1': '&#xe90a;',
+            'alignment-2': '&#xe90b;',
+            'alignment-3': '&#xe90c;',
+            'alignment-4': '&#xe90d;',
+            'alignment-5': '&#xe90e;',
+            'sort-vertical': '&#xe90f;',
+            'italics': '&#xe910;',
+            'bold': '&#xe911;',
+            'strike': '&#xe912;',
+            'underline': '&#xe913;',
+            'numbers': '&#xe914;',
+            'superscript': '&#xe915;',
+            'bullet-list': '&#xe916;',
+            'shrink': '&#xe917;',
+          '0': 0
+        };
+        delete icons['0'];
+        window.icomoonLiga = function (els) {
+            var classes,
+                el,
+                i,
+                innerHTML,
+                key;
+            els = els || document.getElementsByTagName('*');
+            if (!els.length) {
+                els = [els];
+            }
+            for (i = 0; ; i += 1) {
+                el = els[i];
+                if (!el) {
+                    break;
+                }
+                classes = el.className;
+                if (/icon-/.test(classes)) {
+                    innerHTML = el.innerHTML;
+                    if (innerHTML && innerHTML.length > 1) {
+                        for (key in icons) {
+                            if (icons.hasOwnProperty(key)) {
+                                innerHTML = innerHTML.replace(new RegExp(key, 'g'), icons[key]);
+                            }
+                        }
+                        el.innerHTML = innerHTML;
+                    }
+                }
+            }
+        };
+        window.icomoonLiga();
+    }
+}());
